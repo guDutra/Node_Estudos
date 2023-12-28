2cd9794abb14299096c94e3e806949acc9cfc14f
guDutra <gudutra05@outlook.com>
2023-12-27 22:25:01 -0300

    feito até insert dos artigos e conversao timezone

--- CursoUdemy/BlogAdministrativo/articles/ArticlesController.js
+++ CursoUdemy/BlogAdministrativo/articles/ArticlesController.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const router = express.Router();
-
-router.get("/articles", (req, res) => {
+const Category = require('../categories/Category');
+const Article = require('./Article');
+const slugify = require('slugify');
+router.get("/admin/articles", (req, res) => {
     res.send("Rota artigo");
 });
 
 router.get("/admin/articles/new", (req, res) => {
-    res.send("Nova artigo");
+    Category.findAll().then(categories => {
+        res.render('admin/articles/new', { categories: categories });
+    }).catch((error) => { console.log('Error loading categories from database on new article page - ', error) });
+
 });
 
+router.post('/articles/save', (req, res) => {
+    var title = req.body.title;
+    var categoryId = req.body.categoryId;
+    var body = req.body.body;
+
+    Article.create({
+        title: title,
+        body: body,
+        slug: slugify(title),
+        categoryId: categoryId
+    }).then(() => {
+        res.redirect('/admin/articles');
+    })
+});
 module.exports = router;
--- CursoUdemy/BlogAdministrativo/database/connection.js
+++ CursoUdemy/BlogAdministrativo/database/connection.js
@@ -3,7 +3,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 const connection = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
     host: process.env.MYSQL_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00'
 });
 
 module.exports = connection;
