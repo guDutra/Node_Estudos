e3a77519eaac447212f4f9909417ff0d08dd31b1
guDutra <gudutra05@outlook.com>
2023-12-29 19:31:34 -0300

    navbar categorias feito

--- CursoUdemy/BlogAdministrativo/index.js
+++ CursoUdemy/BlogAdministrativo/index.js
@@ -10,6 +10,7 @@ const ArticleModel = require('./articles/Article');
 const CategoryModel = require('./categories/Category');
 const Article = require('./articles/Article');
 const { where } = require('sequelize');
+const Category = require('./categories/Category');
 
 //Ejs
 app.set('view engine', 'ejs');
@@ -34,7 +35,10 @@ app.get("/", (req, res) => {
             ['id', 'DESC']
         ]
     }).then(articles => {
-        res.render("index.ejs", { articles: articles });
+        Category.findAll().then(categories => {
+            res.render("index.ejs", { articles: articles, categories: categories });
+        })
+
     })
 
 });
@@ -44,15 +48,39 @@ app.get('/:slug', (req, res) => {
         where: { slug: slug }
     }).then(article => {
         if (article == undefined) {
-            res.render('/');
+            res.redirect('/');
         } else {
-            res.render('article', { article: article });
+            Category.findAll().then(categories => {
+                res.render('article', { article: article, categories: categories });
+            })
         }
     }).catch(error => {
         console.log('Error selecting the slug of the article on database - ', error);
         res.redirect('/');
     });
 })
+
+app.get('/category/:slug', (req, res) => {
+    var slug = req.params.slug;
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{ model: Article }]
+    }).then(category => {
+        if (category != undefined) {
+            Category.findAll().then(categories => {
+                res.render('index', {articles: category.articles, categories: categories});
+            })
+        } else {
+            res.redirect('/')
+        }
+    }).catch(err => {
+        console.log('Error loading categories from the database on category page - ', err)
+        res.redirect('/')
+    }
+    )
+});
 app.use('/', categoriesController);//Aqui pode ser colocado prefixo para rota/url
 app.use('/', articlesController);
 
