60d7747830f8a2bd7c1e3f251a87053582831fc5
guDutra <gudutra05@outlook.com>
2024-01-19 00:13:20 -0300

    aula de v-for finalizada

--- CursoUdemy/VueJSModulo/guiaclientes/src/App.vue
+++ CursoUdemy/VueJSModulo/guiaclientes/src/App.vue
@@ -1,26 +1,82 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    
+    <div v-for="(cliente, index) in clientes" :key="cliente.id" >
+      <h5>{{ index }}</h5>
+      <ClienteComponent :cliente="cliente"/>
+      <hr>
+      <h3>Edição:</h3>
+      <input type="text"  v-model="cliente.nome">
+      <input type="email"  v-model="cliente.email">
+    </div>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ClienteComponent from "./components/Cliente.vue";
 
 export default {
   name: 'App',
+  data() {
+    return {
+      clienteVictor: {
+        nome: 'Victor',
+        numero: '054-42424-4242',
+        email: 'victor@gmail.com',
+        idade: 23,
+        descricao: 'dcoarmoerffe'
+      },
+      clientes: [{
+        id: 2,
+        nome: 'Gustavo Dutra',
+        numero: '054-98432-2121',
+        email: 'gudutra05@outlook.com',
+        idade: 43,
+        descricao: 'alto baixo gradned deajfdpowef'
+      },
+      {
+        id: 22,
+        nome: 'Joao ',
+        numero: '054-98432-2121',
+        email: 'gudutra05@outlook.com',
+        idade: 18,
+        descricao: 'alto baixo gradned deajfdpowef'
+      },
+      {
+        id: 45,
+        nome: 'Leonardo',
+        numero: '054-98432-2121',
+        email: 'gudutra05@outlook.com',
+        idade: 20,
+        descricao: 'alto baixo gradned deajfdpowef'
+      },
+      {
+        id: 29,
+        nome: 'Bruno',
+        numero: '054-98432-2121',
+        email: 'gudutra05@outlook.com',
+        idade: 16,
+        descricao: 'alto baixo gradned deajfdpowef'
+      },
+      {
+        id: 98,
+        nome: 'Rafael',
+        numero: '054-98432-2121',
+        email: 'gudutra05@outlook.com',
+        idade: 24,
+        descricao: 'alto baixo gradned deajfdpowef'
+      }
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    ClienteComponent
+
+
+
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
